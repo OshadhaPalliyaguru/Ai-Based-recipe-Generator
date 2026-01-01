4d9bffaf81b1c5bc0a9c857c78311570528899ad
Oshadha <moshadha23@gmail.com>
2026-01-01 23:50:14 +0530

    add Form component to App for ingredient input functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import HeaderCom from "/components/HeaderCom.jsx"
+import Form from '../components/Form'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
     <HeaderCom />
+    <Form />
     </>
   )
 }
