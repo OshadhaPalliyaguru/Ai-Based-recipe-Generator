8a8e8289c9a1930e3a6f8a9516ea6fe07776c34f
Oshadha <moshadha23@gmail.com>
2026-01-01 23:49:57 +0530

    add Form component for ingredient input functionality

---
+++ components/Form.jsx
@@ -0,0 +1,16 @@
+export default function Form(){
+    return(
+        
+        <div className="ingrediet_form">
+         <input type="text" className="ingredient_txtfeld"
+        placeholder="Enter the ingredents"
+        name="ingredients"
+        />
+        <button className="ingredient_button">Add Ingredient</button>
+        </div>
+       
+        
+        
+        
+    )
+}
