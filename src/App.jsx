e254e425da25785fa3dfcc63192eecfb0275a9b8
Oshadha <moshadha23@gmail.com>
2026-01-01 23:31:56 +0530

    remove unused imports and update App component to include HeaderCom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HeaderCom from "/components/HeaderCom.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     <h1>Welcome to Recipe generator</h1>
+    <HeaderCom />
     </>
   )
 }
