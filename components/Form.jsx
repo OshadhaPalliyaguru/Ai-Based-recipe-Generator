b51c09ec13fb70de836705b7e734c603a9dea494
Oshadha <moshadha23@gmail.com>
2026-01-02 23:05:44 +0530

    refactor Form component for improved structure and ingredient display

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,16 +1,33 @@
 export default function Form(){
+
+    const ingredients = ["eggs", "milk", "flour", "sugar"];
+
+
+    const ingredientList = ingredients.map(()=>(
+        <li>{ingredients}</li>
+    ))
+
+
     return(
+        <div className = "form_container">
         
         <div className="ingrediet_form">
-         <input type="text" className="ingredient_txtfeld"
+
+         <input type="text" 
+         className="ingredient_txtfeld"
         placeholder="Enter the ingredents"
         name="ingredients"
         />
+
+       
         <button className="ingredient_button">Add Ingredient</button>
         </div>
-       
+
+        <ul className="ingredient_list">
+            {ingredientList}
+        </ul>
         
-        
+        </div>
         
     )
 }
